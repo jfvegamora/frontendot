07298215376750ffa5984bb84e23f32611e49734
jfvegamora <jfvegamora@gmail.com>
2024-01-17 14:40:55 -0300

    .

--- src/presentation/views/mantenedores/MOTHistorica.tsx
+++ src/presentation/views/mantenedores/MOTHistorica.tsx
@@ -161,8 +161,15 @@ export enum EnumGrid {
   numero_guia = 122,
   numero_factura = 123,
   folio_interno_mandante = 124,
-  total = 125,
-  observaciones = 126
+  reporte_interno_mandante = 125,
+  total = 126,
+  observaciones = 127,
+  nombre_logo = 128,
+  imprime_qr = 129,
+  imprime_ticket = 130,
+  titulo1_ticket = 131,
+  titulo2_ticket = 132,
+  titulo3_ticket = 133,
 }
 
 
