61547209a28f7c32c7574e2bc8ab54aa0d738760
jfvegamora <jfvegamora@gmail.com>
2024-01-19 09:27:11 -0300

    .

--- src/presentation/utils/table_head_utils.tsx
+++ src/presentation/utils/table_head_utils.tsx
@@ -13,7 +13,7 @@ export const table_head_OT_diaria2 = [
     key: "folio",
     visible: true,
     alignment: "text-center",
-    width: "w-[10rem]",
+    width: "w-[5rem]",
     background: true
   },
   {
