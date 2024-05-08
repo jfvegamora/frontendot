8636b4d3d1d71ba4c0578daf8cab4afab0ad5ec2
jfvegamora <jfvegamora@gmail.com>
2024-05-08 10:45:14 -0400

    .

--- src/presentation/components/LogoImagenImpresion.tsx
+++ src/presentation/components/LogoImagenImpresion.tsx
@@ -9,7 +9,7 @@ const LogoImagenImpresion:React.FC<ILogoImagenImpresion> = ({
     imagen64
 }) => {
   return (
-    <img src={`data:image/jpeg;base64, ${imagen64}`} alt="Imagen"  className='w-[90%] ml-2'/>
+    <img src={`data:image/jpeg;base64, ${imagen64}`} alt="Imagen"  className='w-[100%]'/>
 )
 }
 
